36467842ad1a356f610ad71d6a07a88cff17936b
Jordan VanderZwaag <jvanderzwaag@enova.com>
2015-10-02 00:15:35 -0500

    basic dashboard event display

--- app/routes/volunteer/dashboard.js
+++ app/routes/volunteer/dashboard.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController(controller, model){
+    this._super(controller, model);
+    var eventsPromise = this.get('store').findAll('event');
+    eventsPromise.then(json => {
+      controller.set('events', json);
+    });
+  }
 });
